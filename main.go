2769bdc3874068c48531e160eeddac57daf3f121
ImranShd <imran@thankyo.net>
2019-08-08 12:21:41 +0500

    adding first sommit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var articles []Article
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Welcome to Homepage")
+	fmt.Println("Endpoint hit: homepage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Endpoint hit: Articles All")
+
+	json.NewEncoder(w).Encode(articles)
+
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "%+v", string(reqBody))
+
+}
+
+func returnSingleArticles(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+
+}
+
+func handleRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homepage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticles)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+
+	a := Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"}
+	b := Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"}
+
+	articles = append(articles, a)
+	articles = append(articles, b)
+
+	handleRequests()
+}
